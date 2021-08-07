cf45a95c1c19b1e09d6b65f281c9216a4ac77039
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2021-08-07 20:46:50 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,46 @@
-import React, { Component, useState } from "react";
-import "../styles/App.css";
+import React from "react";
 
-const App = (props) => {
-  const { slides } = props;
+const App = () => {
+  let cityList = [
+    { name: "Goa", country: "India" },
+    { name: "Amsterdam", country: "Netherlands" },
+    { name: "New York", country: "USA" },
+    { name: "Darjeeling", country: "India" },
+    { name: "Tokyo", country: "Japan" },
+    { name: "Lonavala", country: "India" },
+    { name: "Brandenburg Gate", country: "Germany" },
+    { name: "Reichstag Building", country: "Germany" },
+    { name: "Museum Island", country: "Germany" },
+    { name: "Munnar", country: "India" },
+    { name: "Leh Ladakh", country: "India" },
+    { name: "Goa", country: "India" },
+    { name: "Agra", country: "India" },
+    { name: "Dalhousie", country: "India" },
+    { name: "Coorg", country: "India" },
+    { name: "Rome", country: "Italy" },
+    { name: "Milan", country: "Italy" },
+    { name: "Venice", country: "Italy" },
+    { name: "Varanasai", country: "India" },
+    { name: "Jaipur", country: "India" },
+    { name: "The Hofburg", country: "Austria" },
+    { name: "Belvedere Palace", country: "Austria" },
+    { name: "St. Stephen Cathedral", country: "Austria" },
+    { name: "Kahna National Park", country: "India" },
+    { name: "Amritsar", country: "India" },
+    { name: "Mussoorie", country: "India" },
+    { name: "Mount Abu", country: "India" },
+    { name: "Tirupati", country: "India" }
+  ];
 
-  const [index, setIndex] = useState(0);
-
-  let title = slides[index].title;
-  let text = slides[index].text;
-
-  const nextClick = () => {
-    if (index <= slides.length || slides.length === 1) {
-      console.log("nextclick", index);
-      let newIndex = index + 1;
-      setIndex(newIndex);
-    }
-  };
-  const prevClick = () => {
-    if (index >= 0 && index <= slides.length) {
-      console.log("preClick", index);
-      let newIndex = index - 1;
-      setIndex(newIndex);
-    }
-  };
-
-  const restartClick = () => {
-    setIndex(0);
-  };
-
+  const filteredCity = cityList.filter((el) => el.country === "India");
   return (
-    <>
-      <h1 data-testid="title">{title}</h1>
-      <p data-testid="text">{text}</p>
-
-      <button
-        data-testid="button-prev"
-        disabled={index === 0 ? true : false}
-        onClick={prevClick}
-      >
-        Prev
-      </button>
-
-      <button
-        data-testid="button-next"
-        disabled={index === slides.length - 1 ? true : false}
-        onClick={nextClick}
-      >
-        Next
-      </button>
-
-      <button
-        data-testid="button-restart"
-        disabled={index === 0 ? true : false}
-        onClick={restartClick}
-      >
-        Restart
-      </button>
-    </>
+    <div id="main">
+      <ol>
+        {filteredCity.map((el, id) => (
+          <li key={location + (id + 1)}>{el.name}</li>
+        ))}
+      </ol>
+    </div>
   );
 };
 
