3161360740eb384eaae6c879718070bead1757f0
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2021-08-07 20:54:47 +0530

    button action

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,31 @@
-import React from "react";
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+// import RenderPara from "./RenderPara";
 
-const App = () => {
-  let cityList = [
-    { name: "Goa", country: "India" },
-    { name: "Amsterdam", country: "Netherlands" },
-    { name: "New York", country: "USA" },
-    { name: "Darjeeling", country: "India" },
-    { name: "Tokyo", country: "Japan" },
-    { name: "Lonavala", country: "India" },
-    { name: "Brandenburg Gate", country: "Germany" },
-    { name: "Reichstag Building", country: "Germany" },
-    { name: "Museum Island", country: "Germany" },
-    { name: "Munnar", country: "India" },
-    { name: "Leh Ladakh", country: "India" },
-    { name: "Goa", country: "India" },
-    { name: "Agra", country: "India" },
-    { name: "Dalhousie", country: "India" },
-    { name: "Coorg", country: "India" },
-    { name: "Rome", country: "Italy" },
-    { name: "Milan", country: "Italy" },
-    { name: "Venice", country: "Italy" },
-    { name: "Varanasai", country: "India" },
-    { name: "Jaipur", country: "India" },
-    { name: "The Hofburg", country: "Austria" },
-    { name: "Belvedere Palace", country: "Austria" },
-    { name: "St. Stephen Cathedral", country: "Austria" },
-    { name: "Kahna National Park", country: "India" },
-    { name: "Amritsar", country: "India" },
-    { name: "Mussoorie", country: "India" },
-    { name: "Mount Abu", country: "India" },
-    { name: "Tirupati", country: "India" }
-  ];
+class App extends Component {
+    constructor(props) {
+		super(props);
+		this.state={
+			text: "",
+			id=""
+		}
+	};
+	setText(){
+		this.setState({
+			text:"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy",
+			id:"para"
+		})
+	}
+    render() {
+    	return(
+    		<div id="main">
+				  {/* Do not remove this main div!!   */}
+				  <button id="click" onClick={()=>this.setText()}>RenderPara</button>
+				  <p id={this.state.id}>{this.state.text}</p>
+    		</div>
+    	);
+    }
+}
 
-  const filteredCity = cityList.filter((el) => el.country === "India");
-  return (
-    <div id="main">
-      <ol>
-        {filteredCity.map((el, id) => (
-          <li key={location + (id + 1)}>{el.name}</li>
-        ))}
-      </ol>
-    </div>
-  );
-};
 
 export default App;
